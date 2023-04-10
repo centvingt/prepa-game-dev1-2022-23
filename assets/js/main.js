75a40718a1bbd71ea5d489f49e82a23a9fc3e3e5
Vincent Caronnet <120@120.design>
2023-04-10 22:33:17 +0200

    Correction de l’affichage du canvas

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,16 @@ import { Game } from './modules/game.js'
 
 new Game()
 
+const mobileUi = document.querySelectorAll('.mobile-ui')
 if (
   !/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
     navigator.userAgent
   )
-)
+) {
   document.querySelector('.desktop-instructions').style.display = 'block'
+  for (const ui of mobileUi) ui.style.display = 'none'
+  document.querySelector('.game').classList.add('desktop')
+}
 
 // if (navigator.serviceWorker) {
 //   navigator.serviceWorker
